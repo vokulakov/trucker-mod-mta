2b5674ab7a9b4422da49c9f1d20818e99f76088f
Vladimir Kulakov <vladimir.kulakov@whitetigersoft.ru>
2024-08-05 11:23:54 +0300

    Commit

--- [ui]/tmtaDashboard/client/Freeroam/WalkstyleGUI.lua
+++ [ui]/tmtaDashboard/client/Freeroam/WalkstyleGUI.lua
@@ -1,17 +1,21 @@
 WalkstyleGUI = {}
 WalkstyleGUI.visible = false
 
-local width, height
-local posX, posY
+local width, height = 225, 300
+local posX, posY = (sDW-width) /2, (sDH-height) /2
 
 function WalkstyleGUI.create()
-    width, height = 225, 300
-    posX, posY = 20, 20
-
     WalkstyleGUI.wnd = guiCreateWindow(sW*((posX) /sDW), sH*((posY) /sDH), sW*((width) /sDW), sH*((height) /sDH), 'Выбери стиль походки', false)
     WalkstyleGUI.wnd.sizible = false
     WalkstyleGUI.wnd.movable = false
-    WalkstyleGUI.wnd.visible = false
+
+    WalkstyleGUI.walkstyleList = guiCreateGridList(0, 30, sW*((width-15) /sDW), sH*((height-60) /sDH), false, WalkstyleGUI.wnd)
+	guiGridListSetSortingEnabled(WalkstyleGUI.walkstyleList, false)
+	local column = guiGridListAddColumn(WalkstyleGUI.walkstyleList, 'Стили походки', 0.8)
+
+    WalkstyleGUI.btnClose = guiCreateButton(0, sH*((30) /sDH), sW*((width-15) /sDW), sH*((30) /sDH), 'Закрыть', false, WalkstyleGUI.wnd)
+    guiSetFont(WalkstyleGUI.btnClose, Font['RR_10'])
+    addEventHandler('onClientGUIClick', WalkstyleGUI.btnClose, WalkstyleGUI.setVisible, false)
 
     Dashboard.addWindow(WalkstyleGUI.wnd)
 end
---
+++ [ui]/tmtaDashboard/shared/Walkstyle.lua
@@ -0,0 +1,28 @@
+Walkstyle = {}
+
+Walkstyle.list = {
+	[54]  	= 'Default',
+	[55]  	= 'Default Fat',
+	[56]  	= 'Default Muscular',
+	[69]  	= 'Sneak',
+	[118]  	= 'Man',
+	[119] 	= 'Shuffle',
+	[120]  	= 'Old Man',
+	[121]  	= 'Gang One',
+	[122]  	= 'Gang Two',
+	[124]  	= 'Fat Man',
+	[123]  	= 'Old Fat Man',
+	[125]  	= 'Jogger',
+	[126]  	= 'Drunk',
+	[127]  	= 'Blind Man',
+	[128]  	= 'SWAT Team',
+	[129]  	= 'Woman',
+	[130]  	= 'Shopper',
+	[131]  	= 'Busy Woman',
+	[132]  	= 'Sexy Woman',
+	[133]  	= 'Hooker',
+	[134]  	= 'Old Woman',
+	[135]  	= 'Fat Woman',
+	[136]  	= 'Jogger Woman',
+	[137]  	= 'Old Fat Woman',
+}
