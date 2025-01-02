37482686f5dfc3c85dc61683b87d2eb53aa38254
Vladimir Kulakov <vokulakov01@gmail.com>
2025-01-02 23:38:22 +0300

    tmtaTrucker: update config

--- [core]/tmtaTrucker/shared/Config.lua
+++ [core]/tmtaTrucker/shared/Config.lua
@@ -8,25 +8,25 @@ Config.DRIVER_EXP_PER_WEIGHT        = 0.000126  -- коэффицент опыт
 Config.RENT_TIME = 60 -- время аренды ТС в минутах
 Config.ORDER_LIST_UPDATE_TIME = 60 -- время обновления списка заказов
 
-Config.FORFEIT_PERCENT = 15 -- неустойка за отмену заказа (в %)
+Config.FORFEIT_PERCENT = 17 -- неустойка за отмену заказа (в %)
 
 -- Список транспорта для аренды
 Config.TRUCK_RENT = {
     { model = 'izh_2715', price = 200, level = 1 },
     { model = 'izh_2717', price = 450, level = 1 },
     { model = 'izh_oda_2717', price = 750, level = 1 },
-    { model = 'raf_2203', price = 900, level = 1 },
-    { model = 'vw_transporter', price = 1300, level = 1 },
 
+    { model = 'raf_2203', price = 1050, level = 2 },
+    { model = 'vw_transporter', price = 1400, level = 2 },
     { model = 'gazel_3302', price = 2250, level = 2 },
-    { model = 'mercedes_benz_sprinter', price = 4500, level = 2 },
+    { model = 'mercedes_benz_sprinter', price = 3500, level = 2 },
 
-    { model = 'gaz_3307', price = 2250, level = 3 },
-    { model = 'gazon_next', price = 2250, level = 3 },
+    { model = 'gaz_3307', price = 4750, level = 3 },
+    { model = 'gazon_next', price = 5300, level = 3 },
 
-    { model = 'kamaz_54115', price = 9500, level = 4 },
-    { model = 'iveco_stralis_500', price = 7000, level = 4 },
-    { model = 'scania_r700', price = 9500, level = 4 },
+    { model = 'kamaz_54115', price = 7800, level = 4 },
+    { model = 'iveco_stralis_500', price = 8800, level = 4 },
+    { model = 'scania_r700', price = 9100, level = 4 },
 
     { model = 'volvo_fh12', price = 12500, level = 6 },
 
