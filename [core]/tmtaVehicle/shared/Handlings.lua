abe948c34049885f60ad7e21a3254d0c1defdcb2
Vladimir Kulakov <vokulakov01@gmail.com>
2025-01-04 23:04:55 +0300

    tmtaVehicle: добавил handling для gaz_3307

--- [core]/tmtaVehicle/shared/Handlings.lua
+++ [core]/tmtaVehicle/shared/Handlings.lua
@@ -3,6 +3,7 @@ local handlingsTable = {}
 handlingsTable['izh_2715'] = "SADLER 1700 4500 2.7 0 0 -0.05 75 0.65 0.7 0.5 5 70 5 20 r d 8.5 0.5 false 35 0.8 0.08 3 0.25 -0.15 0.4 0.4 0.26 0.2 26000 200040 104004 0 1 0"
 handlingsTable['izh_oda_2717'] = "FBITRUCK 4000 10000 2 0 0 -0.2 85 0.65 0.85 0.54 5 170 10 25 r d 6 0.4 false 30 0.8 0.1 0 0.3 -0.12 0.4 0 0.32 0.16 40000 4001 0 0 1 13"
 handlingsTable['gazel_3302'] = "MULE 3500 14000 4 0 0 0.1 80 0.55 0.85 0.46 5 140 7.2 20 r d 4.5 0.6 false 30 2 0.07 5 0.3 -0.15 0.5 0 0.46 0.53 22000 4088 4048400 0 3 0"
+handlingsTable['gaz_3307'] = "FLATBED 8500 48804.199 2 0 0 -0.2 90 0.65 0.8 0.4 5 110 6 78 r d 3.5 0.4 false 30 1.4 0.06 0 0.47 -0.1 0.55 0 0.62 0.43 10000 4008 200 0 1 2"
 
 function getVehicleHandlingConfig(vehicleName)
     local handlingString = handlingsTable[vehicleName]
