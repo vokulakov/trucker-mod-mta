bc7de633064533928e39cdaf445f0f7b2f71017d
Vladimir Kulakov <vokulakov01@gmail.com>
2025-01-05 20:16:06 +0300

    Рефакторинг кода замены текстур зимнего мода

--- [graphics]/[winter]/winter_mod/server/server.lua
+++ [graphics]/[winter]/winter_mod/server/server.lua
@@ -1,16 +1,19 @@
-addEventHandler('onPlayerContact', getRootElement(), function(prev, current)
-	if (current) then
-		if getElementType(current) ~= 'object' then return end
-		if getElementModel(current) == 2263 then
-			setPedWalkingStyle(source, 138) 
-		end
-	else
-		if getElementType(prev) ~= 'object' then return end
-		if getElementModel(prev) == 2263 then
-			setPedWalkingStyle(source, 0) 
+addEventHandler('onPlayerContact', root, 
+	function(prev, current)
+		if (current) then
+			if getElementType(current) ~= 'object' then return end
+			if getElementModel(current) == 2263 then
+				setPedWalkingStyle(source, 138) 
+			end
+		else
+			if getElementType(prev) ~= 'object' then return end
+			if getElementModel(prev) == 2263 then
+				setPedWalkingStyle(source, 0) 
+			end
 		end
 	end
-end)
+)
+
 --[[
 addEventHandler('onPlayerVehicleEnter', getRootElement(), function(veh, seat)
 	if seat ~= 0 then return end
